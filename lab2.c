9b69867950b4603481b7e034c7915630d27ea71f
Овчинникова София Дмитриевна <ri57s11@awp02.csc.local>
2016-04-05 11:11:40 +0600

    correction format

--- lab2.c
+++ lab2.c
@@ -3,30 +3,29 @@
 void qwrt(int a, int b, int c)
 {
     int d, x1, x2;
-    d = b*b - 4*a*c;
-	if( d < 0 )
+    d = b * b - 4 * a * c;
+	if (d < 0)
     {
-        printf ("\nNo roots\n");
+        printf("\nNo roots\n");
     }
-    else if( d == 0 )
+    else if (d == 0)
     {
-        x1 = -b/(2*a);
-        printf ("\nRoot: %d\n", x1);
+        x1 = -b / (2 * a);
+        printf("\nRoot: %d\n", x1);
     }
-	 else if( d > 0 )
+	else if (d > 0)
     {
-        x1 = (-b + sqrt(d))/(2*a);
-        x2 = (-b - sqrt(d))/(2*a);
-        printf ("\nRoots: %d %d\n", x1, x2);
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        printf("\nRoots: %d %d\n", x1, x2);
     }
-
 }
 
 int main()
 {
     int a, b, c;
-    printf ("Enter a, b, c: \n");
-    scanf ("%d%d%d", &a, &b, &c);
+    printf("Enter a, b, c: \n");
+    scanf("%d%d%d", &a, &b, &c);
     qwrt(a, b, c);
     return 0;
 }
