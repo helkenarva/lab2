61355a53fda6c7ddd8e0c41e0c33a2a69853823a
Овчинникова София Дмитриевна <ri57s11@awp02.csc.local>
2016-04-05 10:50:55 +0600

    Add answer if D=0

--- lab2.c
+++ lab2.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+void qwrt(int a, int b, int c)
+{
+    int d, x1, x2;
+    d = b*b - 4*a*c;
+	if( d < 0 )
+    {
+        printf ("\nNo roots\n");
+    }
+    else if( d == 0 )
+    {
+        x1 = -b/(2*a);
+        printf ("\nRoot: %d\n", x1);
+    }
+
+}
 
 int main()
 {
